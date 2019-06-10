d6f56592b9135be44cb74e7d24637caeeeb2e5f4
gabriel.felicidade <gabriel.felicidade@etec.sp.gov.br>
2019-06-10 13:44:41 -0300

    Adicionado método delete e atualização nas rotas

--- restify01/users/users.router.ts
+++ restify01/users/users.router.ts
@@ -2,52 +2,46 @@ import * as restify from 'restify';
 
 import { Router } from '../common/router';
 import { User } from './users.model';
-import { response } from 'spdy';
 
 class UsersRouter extends Router {
+
+    constructor(){
+        super();
+        this.on('beforeRender', 
+                (document) => {
+                    document.password = undefined;
+                });
+    }
+
     applyRoutes(application: restify.Server) {
 
         application.get('/users', (req, res, next) => {
-            User.find().then(
-                (users) => {
-                    res.json(users);
-                    return next();
-                });
+            User.find().then(this.render(res, next));
         });
 
         application.get('/users/:id', (req, res, next) => {
-            User.findById(req.params.id).then(
-                (user) => {
-                    if (user) {
-                        res.json(user);
-                        return next();
-                    }
-
-                    res.send(404);
-                    return next();
-                });
+            User.findById(req.params.id).then(this.render(res, next));
         });
 
         application.post('/users', (req, res, next) => {
             let user = new User(req.body);
-            user.save().then(
-                (user) => {
-                    user.password = undefined;
-                    res.json(user);
-                    return next();
-                });
+            user.save().then(this.render(res, next));
         });
 
         application.put('/users/:id', (req, res, next) => {
             const options = { new: true };
-            User.findByIdAndUpdate(req.params.id, req.body, options).then(
-                (user) => {
-                    if(user){
-                        res.json(user);
-                        return next();
-                    }
+            User.findByIdAndUpdate(req.params.id, req.body, options)
+                .then(this.render(res, next));
+        });
 
-                    res.send(404);
+        application.del('/users/:id', (req, res, next) => {
+            User.deleteOne({ _id: req.params.id }).exec().then(
+                (result: any) => {
+                    if(result.deletedCount){
+                        res.send(204);
+                    }else{
+                        res.send(404);
+                    }
                     return next();
                 });
         });
